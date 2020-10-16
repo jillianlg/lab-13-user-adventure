bd1b3a2c08f84a771eeadd29303e2e3f4d760abd
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-15 23:14:13 -0700

    step 6 - find adventures by id on adventures.js

--- adventures/adventures.js
+++ adventures/adventures.js
@@ -1,8 +1,11 @@
+import { adventures } from '../adventures.js';
 import { findById } from '../utils.js';
 
 const searchParams = new URLSearchParams(window.location.search);
 
 // console.log(searchParams.get('id'));
+
+const id = searchParams.get('id');
 
 const adventure = findById(adventures, id);
 
