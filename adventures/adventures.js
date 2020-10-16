579802a30a7a933a9b939f278f555f451cdc5e41
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-16 08:19:48 -0700

    Step 6 - append and log choices to adventures.js

--- adventures/adventures.js
+++ adventures/adventures.js
@@ -1,12 +1,22 @@
 import adventures from '../data.js';
 import { findById } from '../utils.js';
 
+const section = document.querySelector('section');
+
 const searchParams = new URLSearchParams(window.location.search);
 
-console.log(searchParams.get('id'));
+// console.log(searchParams.get('id'));
 
 const id = searchParams.get('id');
 
 const adventure = findById(adventures, id);
 
-console.log(adventure);
+// console.log(adventure);
+
+const h2 = document.createElement('h2');
+
+h2.textContent = adventure.title;
+
+section.appendChild(h2);
+
+console.log(adventure.choices);
